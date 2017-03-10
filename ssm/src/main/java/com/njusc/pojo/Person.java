2dab77138aec75e8ace3ede86615a6752cfc6532
YuEcho <yhyecho@gmail.com>
2017-03-10 17:28:58 +0800

    add Person pojo

---
+++ ssm/src/main/java/com/njusc/pojo/Person.java
@@ -0,0 +1,16 @@
+package com.njusc.pojo;
+
+/**
+ * Created by Echo on 17/3/10.
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
